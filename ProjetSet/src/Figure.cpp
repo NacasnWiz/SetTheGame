a5a9494ce6753fcd976c2c77d341b37e534fb736
Yohan Ponthus <yohanponthus@gmail.com>
2023-03-03 11:23:46 +0100

    Better constructor by ID for Figure : static_cast the enums

--- ProjetSet/src/Figure.cpp
+++ ProjetSet/src/Figure.cpp
@@ -7,41 +7,10 @@ Figure::Figure(Color color, Shape shape, Filling filling, int number)
 }
 
 Figure::Figure(int ID)
-	: m_ID{ ID }, m_number { ID % 3 }
+	: m_ID{ ID },
+	m_color{ static_cast<Color>((ID % 81) / 27) },
+	m_shape{ static_cast<Shape>((ID % 27) / 9) },
+	m_filling{ static_cast<Filling>((ID % 9) / 3) },
+	m_number { ID % 3 }
 {
-	switch ((ID%81) / 27) {
-	case 0:
-		m_color = blue;
-		break;
-	case 1:
-		m_color = red;
-		break;
-	case 2:
-		m_color = green;
-		break;
-	}
-
-	switch ((ID%27) / 9) {
-	case 0:
-		m_shape = triangle;
-		break;
-	case 1:
-		m_shape = square;
-		break;
-	case 2:
-		m_shape = circle;
-		break;
-	}
-
-	switch ((ID%9) / 3) {
-	case 0:
-		m_filling = blank;
-		break;
-	case 1:
-		m_filling = hatched;
-		break;
-	case 2:
-		m_filling = full;
-		break;
-	}
 }
